aac420b21d3688ff9168244a25b18f5be2594e81
AeG-Repres <aegrepresentacao2024@gmail.com>
2025-08-05 18:53:49 -0300

    mudança2

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react( ),tailwindcss()],
+  plugins: [react()],
+  base: './',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  base: './', // <--- ADICIONE OU MANTENHA ESTA LINHA AQUI
 })
+
+
